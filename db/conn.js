d4e89d79f73b40b710de27cb4cc6ffaa03d943f0
gtamaral <rodrigoamaral003@gmail.com>
2023-08-16 16:20:14 -0300

    teste vercel2

--- db/conn.js
+++ db/conn.js
@@ -1,9 +1,11 @@
 const { Sequelize } = require('sequelize')
 
+// url do banco de dados => neon tech
+const DATABASE_URL = 'postgres://gtamaral:xUEgtlk1N2RT@ep-blue-sun-68579555.us-east-2.aws.neon.tech/neondb'
+
 // conection to sequelize with  my database ===> db name, username and password
 // localhos === local db server || dialect === db type
-const sequelize = new Sequelize('nodemvc2', 'root', '', {
-    host: 'localhost',
+const sequelize = new Sequelize(DATABASE_URL, {
     dialect: 'mysql'
 })
 
